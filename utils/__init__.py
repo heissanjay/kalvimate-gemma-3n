092f1869ef64d63855afbb435d2ec0dce004f8e1
heissanjay <heissanjay@gmail.com>
2025-07-27 12:07:35 +0530

    feat: add raw text extraction from data source

---
+++ utils/__init__.py
@@ -0,0 +1,27 @@
+PREFACE_OFFSET = 8
+
+CHAPTERS_PAGE_MAP = {
+    "chapter_1": {"start": 1, "end": 15},
+    "chapter_2": {"start": 16, "end": 31},
+    "chapter_3": {"start": 32, "end": 41},
+    "chapter_4": {"start": 42, "end": 58},
+    "chapter_5": {"start": 59, "end": 73},
+    "chapter_6": {"start": 74, "end": 90},
+    "chapter_7": {"start": 91, "end": 105},
+    "chapter_8": {"start": 106, "end": 123},
+    "chapter_9": {"start": 124, "end": 136},
+    "chapter_10": {"start": 137, "end": 154},
+    "chapter_11": {"start": 155, "end": 172},
+    "chapter_12": {"start": 173, "end": 186},
+    "chapter_13": {"start": 187, "end": 199},
+    "chapter_14": {"start": 200, "end": 217},
+    "chapter_15": {"start": 218, "end": 228},
+    "chapter_16": {"start": 229, "end": 242},
+    "chapter_17": {"start": 243, "end": 260},
+    "chapter_18": {"start": 261, "end": 273},
+    "chapter_19": {"start": 274, "end": 285},
+    "chapter_20": {"start": 286, "end": 299},
+    "chapter_21": {"start": 300, "end": 314},
+    "chapter_22": {"start": 315, "end": 328},
+    "chapter_23": {"start": 329, "end": 333},
+}
